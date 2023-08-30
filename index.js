c4b29ed675add6a2f004c42577f8097dc7007216
writetofahim <writetofahimahammad@gmail.com>
2023-08-30 15:34:25 +0600

    hostname deleted

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const app = require("./app");
 require("dotenv").config();
 const port = process.env.PORT || 8800;
-const hostname = "127.0.0.1";
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -17,6 +16,6 @@ mongoose.connection.on("error", (error) => {
   console.error("Error connecting to MongoDB Atlas:", error);
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Server is running at http://${hostname}:${port}`);
+app.listen(port, () => {
+  console.log(`Server is running at ${port}`);
 });
